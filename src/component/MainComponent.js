1bec96e1dea7dcaa898adc2f48259f4922b941bc
Tri Nguyen <zymethyang@gmail.com>
2019-12-02 19:54:46 +0700

    [fix bug] restore state

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -8,22 +8,26 @@ class MainComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            chart_01: new Array(25),
-            chart_02: new Array(25),
-            chart_03: new Array(25),
-            labels: new Array(25),
+            chart_01: new Array(50),
+            chart_02: new Array(50),
+            chart_03: new Array(50),
+            labels: new Array(50),
             lastActionTime: 0,
-            d_01: 1,
-            d_02: 1,
-            d_03: 1,
-            d_04: 1,
-            d_05: 1,
-            client: undefined
+            d_01: 2,
+            d_02: 2,
+            d_03: 2,
+            d_04: 2,
+            d_05: 2,
+            client: undefined,
+
+            temp: 0,
+            hum: 0,
+            hum_g: 0
         }
     }
 
     componentDidMount() {
-        const options = { username: 'sammy', password: '12345678' };
+        const options = { username: 'sammy', password: '12345678', clientId: `client-${Math.random()}` };
         const client = mqtt.connect('ws://13.229.222.102:8883', options)
         this.setState({ client });
         client.on('connect', function () {
@@ -47,156 +51,69 @@ class MainComponent extends React.Component {
                 labels.push('');
                 labels.shift();
 
-                this.setState({ labels });
-                this.chart01.update();
-            }
-
-            if (jsonMessage.hasOwnProperty('chart_02')) {
                 chart_02.push(parseFloat(jsonMessage.chart_02));
                 chart_02.shift();
                 this.setState({ chart_02 });
-                this.chart02.update();
-            }
 
-            if (jsonMessage.hasOwnProperty('chart_03')) {
-                chart_03.push(parseFloat(jsonMessage.chart_03));
+                chart_03.push(Math.round((parseFloat(jsonMessage.chart_03) / 1024) * 100));
                 chart_03.shift();
                 this.setState({ chart_03 });
-                this.chart03.update();
+
+                this.setState({ labels });
+
+                this.chart01.update();
+
+                this.setState({ temp: parseFloat(jsonMessage.chart_01), hum: parseFloat(jsonMessage.chart_02), hum_g: Math.round((parseFloat(jsonMessage.chart_03) / 1024) * 100) })
+
             }
 
-            /*
             if (jsonMessage.hasOwnProperty('d01')) {
-                console.log(jsonMessage);
-                if (jsonMessage === 0) {
-                    this.setState({ d_01: 2 });
-                } else {
-                    this.setState({ d_01: 1 });
-                }
+                this.setState({ d_01: jsonMessage.d01 });
             }
 
             if (jsonMessage.hasOwnProperty('d02')) {
-                if (jsonMessage.d02 === 0) {
-                    this.setState({ d_02: 2 });
-                } else {
-                    this.setState({ d_02: 1 });
-                }
+                this.setState({ d_02: jsonMessage.d02 });
             }
 
             if (jsonMessage.hasOwnProperty('d03')) {
-                if (jsonMessage.d03 === 0) {
-                    this.setState({ d_03: 2 });
-                } else {
-                    this.setState({ d_03: 1 });
-                }
+                this.setState({ d_03: jsonMessage.d03 });
             }
 
             if (jsonMessage.hasOwnProperty('d04')) {
-                if (jsonMessage.d04 === 0) {
-                    this.setState({ d_04: 2 });
-                } else {
-                    this.setState({ d_04: 1 });
-                }
+                this.setState({ d_04: jsonMessage.d04 });
             }
 
             if (jsonMessage.hasOwnProperty('d05')) {
-                if (jsonMessage.d05 === 0) {
-                    this.setState({ d_05: 2 });
-                } else {
-                    this.setState({ d_05: 1 });
-                }
+                this.setState({ d_05: jsonMessage.d05 });
             }
-            */
+
         })
         //Subcribe database, nếu có dữ liệu cảm biến thay đổi thì lấy về.
 
         const { labels, chart_01, chart_02, chart_03 } = this.state;
 
-        var ctx01 = document.getElementById('canvas01').getContext('2d');
-        var ctx02 = document.getElementById('canvas02').getContext('2d');
-        var ctx03 = document.getElementById('canvas03').getContext('2d');
-
-        this.chart01 = new Chart(ctx01, {
+        var config = {
             type: 'line',
             data: {
                 labels: labels,
                 datasets: [{
-                    label: 'Biểu đồ 01',
+                    label: 'Nhiệt độ',
+                    backgroundColor: 'red',
+                    borderColor: 'red',
                     data: chart_01,
-                    pointRadius: 0,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderWidth: .5
-                }]
-            },
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            ticks: {
-                                display: false
-                            }
-                        }
-                    ]
-                }
-            }
-        });
-
-        this.chart02 = new Chart(ctx02, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Biểu đồ 02',
+                    fill: false,
+                }, {
+                    label: 'Độ ẩm không khí',
+                    fill: false,
+                    backgroundColor: 'blue',
+                    borderColor: 'blue',
                     data: chart_02,
-                    pointRadius: 0,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderWidth: .5
-                }]
-            },
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            ticks: {
-                                display: false
-                            }
-                        }
-                    ]
-                }
-            }
-        });
-
-        this.chart03 = new Chart(ctx03, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Biểu đồ 03',
+                }, {
+                    label: 'Độ ẩm đất',
+                    fill: false,
+                    backgroundColor: 'green',
+                    borderColor: 'green',
                     data: chart_03,
-                    pointRadius: 0,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderWidth: .5
                 }]
             },
             options: {
@@ -210,7 +127,10 @@ class MainComponent extends React.Component {
                     ]
                 }
             }
-        });
+        };
+
+        var ctx01 = document.getElementById('canvas01').getContext('2d');
+        this.chart01 = new Chart(ctx01, config);
     }
 
     onClickControl(index) {
@@ -221,70 +141,95 @@ class MainComponent extends React.Component {
             var { d_01, d_02, d_03, d_04, d_05 } = this.state;
             switch (index) {
                 case 1:
-                    d_01 = d_01 === 1 ? 2 : 1;
-                    this.state.client.publish('FWpfOR6wyKZIoYj', JSON.stringify({ d_01 }))
-                    console.log(JSON.stringify({ d_01 }));
+                    if (d_01 === 1)
+                        d_01 = 2
+                    else
+                        d_01 = 1
+                    this.state.client.publish('FWpfOR6wyKZIoYj', JSON.stringify({ d_01: d_01 }))
                     break;
                 case 2:
-                    d_02 = d_02 === 1 ? 2 : 1;
+                    if (d_02 === 1)
+                        d_02 = 2
+                    else
+                        d_02 = 1
                     this.state.client.publish('FWpfOR6wyKZIoYj', JSON.stringify({ d_02 }))
                     break;
                 case 3:
-                    d_03 = d_03 === 1 ? 2 : 1;
+                    if (d_03 === 1)
+                        d_03 = 2
+                    else
+                        d_03 = 1
                     this.state.client.publish('FWpfOR6wyKZIoYj', JSON.stringify({ d_03 }))
                     break;
                 case 4:
-                    d_04 = d_04 === 1 ? 2 : 1;
+                    if (d_04 === 1)
+                        d_04 = 2
+                    else
+                        d_04 = 1
                     this.state.client.publish('FWpfOR6wyKZIoYj', JSON.stringify({ d_04 }))
                     break;
                 case 5:
-                    d_05 = d_05 === 1 ? 2 : 1;
+                    if (d_05 === 1)
+                        d_05 = 2
+                    else
+                        d_05 = 1
                     this.state.client.publish('FWpfOR6wyKZIoYj', JSON.stringify({ d_05 }))
                     break;
             }
-            this.setState({ d_01, d_02, d_03, d_04, d_05 });
             lastActionTime = moment().unix(new Date());
             this.setState({ lastActionTime });
+            alert("Đã gửi lệnh!")
         } else {
             alert("Chờ 2 giây nữa đi ma tốc độ!")
         }
     }
 
     render() {
-        const { d_01, d_02, d_03, d_04, d_05 } = this.state;
+        const { d_01, d_02, d_03, d_04, d_05, temp, hum, hum_g } = this.state;
+        console.log(d_01, d_02, d_03, d_04, d_05)
         return (
             <div className="container">
-                <div className="card">
-                    <div className="card--container">
-                        <canvas id="canvas01" style={{ width: '100%' }}></canvas>
-                    </div>
+                <div className="card--charts">
+                    <canvas id="canvas01"></canvas>
                 </div>
-                <div className="card">
-                    <div className="card--container">
-                        <canvas id="canvas02" style={{ width: '100%' }}></canvas>
+                <div className="card--info">
+                    <div className="card--info--realtime">
+                        <div className="card--info--realtime--container">
+                            <div className="card--info--realtime--title">Nhiệt độ</div>
+                            <div>{temp}°C</div>
+                        </div>
+
+                        <div className="card--info--realtime--container">
+                            <div className="card--info--realtime--title">Độ ẩm không khí</div>
+                            <div>{hum}%</div>
+                        </div>
+
+                        <div className="card--info--realtime--container">
+                            <div className="card--info--realtime--title">Độ ẩm đất</div>
+                            <div>{hum_g}%</div>
+                        </div>
                     </div>
-                </div>
-                <div className="card">
-                    <div className="card--container">
-                        <canvas id="canvas03" style={{ width: '100%' }}></canvas>
-                    </div>
-                </div>
-                <div className="card">
+
                     <div className="btn--container">
-                        <div style={{ color: d_01 === 2 ? 'purple' : 'black' }} className="btn--style" onClick={() => this.onClickControl(1)}>
-                            ĐÈN
+                        <div className="btn--style" onClick={() => this.onClickControl(1)}>
+                            <img src={d_01 === 2 ? "./on-black.png" : "./on.png"} style={{ width: '100%', height: '100%' }} />
+                            <div className="btn--title">Đèn</div>
                         </div>
-                        <div style={{ color: d_02 === 2 ? 'purple' : 'black' }} className="btn--style" onClick={() => this.onClickControl(2)}>
-                            BƠM
+                        <div className="btn--style" onClick={() => this.onClickControl(2)}>
+                            <img src={d_02 === 2 ? "./on-black.png" : "./on.png"} style={{ width: '100%', height: '100%' }} />
+                            <div className="btn--title">Bơm</div>
                         </div>
-                        <div style={{ color: d_03 === 2 ? 'purple' : 'black' }} className="btn--style" onClick={() => this.onClickControl(3)}>
-                            QUẠT
+                        <div className="btn--style" onClick={() => this.onClickControl(3)}>
+                            <img src={d_03 === 2 ? "./on-black.png" : "./on.png"} style={{ width: '100%', height: '100%' }} />
+                            <div className="btn--title">Quạt</div>
                         </div>
-                        <div style={{ color: d_04 === 2 ? 'purple' : 'black' }} className="btn--style" onClick={() => this.onClickControl(4)}>
-                            PHUN SƯƠNG
+                        <div className="btn--style" onClick={() => this.onClickControl(4)}>
+                            <img src={d_04 === 2 ? "./on-black.png" : "./on.png"} style={{ width: '100%', height: '100%' }} />
+                            <div className="btn--title">Phun sương</div>
                         </div>
-                        <div style={{ color: d_05 === 2 ? 'purple' : 'black' }} className="btn--style" onClick={() => this.onClickControl(5)}>
-                            MÁI CHE
+                        <div className="btn--style" onClick={() => this.onClickControl(5)}>
+                            <img src={d_05 === 2 ? "./on-black.png" : "./on.png"} style={{ width: '100%', height: '100%' }} />
+                            <div className="btn--title">Mái che</div>
                         </div>
                     </div>
                 </div>
